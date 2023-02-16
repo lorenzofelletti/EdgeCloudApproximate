5b243aaff83c3b75babefbd4df1d7950c7eb59be
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2023-02-16 17:17:51 +0100

    refactoring

--- strat_sampler/main.py
+++ strat_sampler/main.py
@@ -1,56 +1,13 @@
-import argparse
 import os
 import sys
 import time
 import pandas as pd
 
+from src.argparser import build_parser
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Sample a csv file by geohash and output to stdout or file (default: stdout)"
-    )
-    parser.add_argument(
-        "input", type=str, help="file to sample", metavar="<input_file>"
-    )
-    parser.add_argument(
-        "--IFS",
-        "-i",
-        type=str,
-        default=",",
-        help="input field separator. (default: ,)",
-        metavar="<sep>",
-    )
-    parser.add_argument(
-        "--sample-percentage",
-        "-s",
-        type=float,
-        default=0.5,
-        help="percentage of entries to sample (default: 0.5)",
-        metavar="<perc>",
-    )
-    parser.add_argument(
-        "--output-file",
-        "-o",
-        type=str,
-        default="stdout",
-        help="file to write to (default: stdout)",
-        metavar="<output_file>",
-    )
-    parser.add_argument(
-        "--sleep",
-        "-t",
-        type=float,
-        default=0.0,
-        help="sleep for x seconds between each entry, 0 means no sleep (default: 0)",
-        metavar="<secs>",
-    )
-    parser.add_argument(
-        "--OFS",
-        type=str,
-        default=None,
-        help="output field separator (default: is IFS)",
-        metavar="<sep>",
-    )
+    parser = build_parser()
 
     # parse arguments
     args = parser.parse_args()
