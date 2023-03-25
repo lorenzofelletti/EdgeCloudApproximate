3e94b1dcd6bb11aa180ae4caaa8eb2ccdc5da0ed
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2023-03-25 13:13:54 +0100

    toml configuration implementation

--- kafka-producer-rs/src/config.rs
+++ kafka-producer-rs/src/config.rs
@@ -1,34 +1,121 @@
-use std::{path::Path, time::Duration, fs};
+use std::{fs, num::NonZeroU64, path::Path, time::Duration};
 
 use toml::Value;
 
-use self::errors::ConfigError;
+use self::errors::{ConfigurationError, ErrorType};
 
 pub mod errors;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Kafka {
     pub brokers: Vec<String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Data<'a> {
     pub source: &'a Path,
     pub msg_sleep_in_ms: Duration,
-    chunk_size: u32,
+    chunk_size: NonZeroU64,
     chunk_sleep_in_ms: Duration,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Config<'a> {
     pub kafka: Kafka,
     pub data: Data<'a>,
 }
 
-fn load_config<'a>(from: &Path) -> Result<Config<'a>, ConfigError> {
-    let contents = fs::read_to_string(from).expect("Config file `producer_config.toml` should exists!");
+fn check_brokers(brokers: &Vec<String>) -> Result<(), ConfigurationError> {
+    Ok(())
+}
 
-    let config: Value = toml::from_str(&contents).map_err(|e| ConfigError::new(e.message()))?;
+pub fn load_config<'a>(from: &Path) -> Result<Config<'a>, ConfigurationError> {
+    let contents =
+        fs::read_to_string(from).expect("Config file `producer_config.toml` should exists!");
 
-    Err(ConfigError::new("Error parsing the configuration"))
+    let config: Value = toml::from_str(&contents)
+        .map_err(|e| ConfigurationError::new(e.message(), ErrorType::Error))?;
+
+    // Parses 'kakfa' table
+    let kafka = config["kafka"]
+        .as_table()
+        .ok_or(ConfigurationError::new("kafka", ErrorType::TableNotFound))?;
+
+    let brokers = kafka["brokers"].as_array().ok_or(ConfigurationError::new(
+        "brokers",
+        ErrorType::KeyNotFoundForTable("kafka"),
+    ))?;
+    let brokers: Vec<String> = brokers
+        .into_iter()
+        .map(|x| x.as_str().unwrap_or_default().to_owned())
+        .collect();
+    if brokers.contains(&String::from("")) {
+        return Err(ConfigurationError::new(
+            "",
+            ErrorType::InvalidValueForKey("brokers"),
+        ));
+    }
+
+    let kafka = Kafka { brokers };
+
+    // Parses 'data' table
+    let data = config["data"]
+        .as_table()
+        .ok_or(ConfigurationError::new("data", ErrorType::TableNotFound))?;
+
+    let source = data["source"].as_str().ok_or(ConfigurationError::new(
+        "source",
+        ErrorType::KeyNotFoundForTable("data"),
+    ))?;
+    let source = Path::new(source);
+
+    let msg_sleep_in_ms: u64 = data["msg_sleep_in_ms"]
+        .as_integer()
+        .ok_or(ConfigurationError::new(
+            "msg_sleep_in_ms",
+            ErrorType::KeyNotFoundForTable("data"),
+        ))?
+        .try_into()
+        .map_err(|e: <u64 as TryFrom<i64>>::Error| ConfigurationError::Error(e.to_string()))?;
+    let msg_sleep_in_ms = Duration::from_millis(msg_sleep_in_ms);
+
+    let chunk_size = data["chunk_size"]
+        .as_integer()
+        .ok_or(ConfigurationError::new(
+            "chunk_size",
+            ErrorType::KeyNotFoundForTable("data"),
+        ))?;
+    let chunk_size: u64 = chunk_size
+        .try_into()
+        .map_err(|e: <u64 as TryFrom<i64>>::Error| ConfigurationError::Error(e.to_string()))?;
+    let chunk_size: NonZeroU64 =
+        chunk_size
+            .try_into()
+            .map_err(|e: <NonZeroU64 as TryFrom<u64>>::Error| {
+                ConfigurationError::Error(e.to_string())
+            })?;
+
+    let chunk_sleep_in_ms: u64 = data["chunk_sleep_in_ms"]
+        .as_integer()
+        .ok_or(ConfigurationError::new(
+            "chunk_sleep_in_ms",
+            ErrorType::KeyNotFoundForTable("data"),
+        ))?
+        .try_into()
+        .map_err(|e: <u64 as TryFrom<i64>>::Error| ConfigurationError::Error(e.to_string()))?;
+    let chunk_sleep_in_ms: Duration = Duration::from_millis(chunk_sleep_in_ms);
+
+    let dataConf = Data {
+        source: source,
+        msg_sleep_in_ms,
+        chunk_size,
+        chunk_sleep_in_ms,
+    };
+
+    let config = Config {
+        kafka,
+        data: dataConf,
+    };
+
+    Ok(config.clone())
 }
--- kafka-producer-rs/src/config/errors.rs
+++ kafka-producer-rs/src/config/errors.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, fmt};
-
+use std::{
+    error::Error,
+    fmt::{self},
+};
+/*
 #[derive(Debug)]
 pub struct ConfigError {
     pub message: String,
@@ -18,5 +21,55 @@ impl Error for ConfigError {}
 impl fmt::Display for ConfigError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Error parsing the configuration: {}", self.message)
+    }
+}*/
+
+#[derive(Debug)]
+pub enum ErrorType<S: Into<String>> {
+    TableNotFound,
+    KeyNotFoundForTable(S),
+    InvalidValueForKey(S),
+    Error,
+}
+
+#[derive(Debug)]
+pub enum ConfigurationError {
+    TableNotFound(String),
+    /// `KeyNotFound(table, key)`
+    KeyNotFound(String, String),
+    /// `InvalidValue(key, value)`
+    InvalidValue(String, String),
+    Error(String),
+}
+
+impl Error for ConfigurationError {}
+
+impl fmt::Display for ConfigurationError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ConfigurationError::TableNotFound(t) => write!(f, "No table '{t}' found."),
+            ConfigurationError::KeyNotFound(t, k) => {
+                write!(f, "No key '{k}' found in table '{t}'.")
+            }
+            ConfigurationError::InvalidValue(k, v) => {
+                write!(f, "Invalid value `{v}` for key '{k}'.")
+            }
+            ConfigurationError::Error(e) => write!(f, "{e}"),
+        }
+    }
+}
+
+impl ConfigurationError {
+    pub fn new<S: Into<String>>(value: S, error_type: ErrorType<S>) -> Self {
+        match error_type {
+            ErrorType::TableNotFound => ConfigurationError::TableNotFound(value.into()),
+            ErrorType::KeyNotFoundForTable(t) => {
+                ConfigurationError::KeyNotFound(t.into(), value.into())
+            }
+            ErrorType::InvalidValueForKey(k) => {
+                ConfigurationError::InvalidValue(k.into(), value.into())
+            }
+            ErrorType::Error => ConfigurationError::Error(value.into()),
+        }
     }
 }
