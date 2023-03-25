cc64a4f1c85c3f75a64717d054daf14634114d48
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2023-03-25 14:15:01 +0100

    updated version

--- kafka-producer-rs/src/config.rs
+++ kafka-producer-rs/src/config.rs
@@ -1,6 +1,6 @@
 use std::{fs, num::NonZeroU64, path::Path, time::Duration};
 
-use toml::Value;
+use toml::{map::Map, Value};
 
 use self::errors::{ConfigurationError, ErrorType};
 
@@ -25,11 +25,52 @@ pub struct Config<'a> {
     pub data: Data<'a>,
 }
 
+/// Checks that the 'brokers' value of the configuration is valid
 fn check_brokers(brokers: &Vec<String>) -> Result<(), ConfigurationError> {
+    if brokers.len() == 0 || brokers.contains(&String::from("")) {
+        return Err(ConfigurationError::new(
+            "",
+            ErrorType::InvalidValueForKey("brokers"),
+        ));
+    }
     Ok(())
 }
 
-pub fn load_config<'a>(from: &Path) -> Result<Config<'a>, ConfigurationError> {
+/// Returns the table from a configuration file, if it exists.
+fn get_table<'a, S: Into<String>>(
+    table_name: S,
+    config: &'a Value,
+) -> Result<&'a Map<String, Value>, ConfigurationError> {
+    let table_name: String = table_name.into();
+    config[table_name]
+        .as_table()
+        .ok_or(ConfigurationError::new("kafka", ErrorType::TableNotFound))
+}
+
+/// Reads an integer key from a table
+fn read_integer_key_from_table<S: Into<String>>(
+    table_name: S,
+    key_name: S,
+    data: &toml::map::Map<std::string::String, Value>,
+) -> Result<i64, ConfigurationError> {
+    let key_name: String = key_name.into();
+    data[&key_name.clone()]
+        .as_integer()
+        .ok_or(ConfigurationError::new(
+            key_name.clone(),
+            ErrorType::KeyNotFoundForTable(table_name.into()),
+        ))
+}
+
+/// Converts i64 to u64, returning a `ConfigurationError` if the conversion fails
+fn from_i64_to_u64(value: i64) -> Result<u64, ConfigurationError> {
+    let res: u64 = value
+        .try_into()
+        .map_err(|e: <u64 as TryFrom<i64>>::Error| ConfigurationError::Error(e.to_string()))?;
+    Ok(res)
+}
+
+pub fn load_config(from: &Path) -> Result<Config<'static>, ConfigurationError> {
     let contents =
         fs::read_to_string(from).expect("Config file `producer_config.toml` should exists!");
 
@@ -37,9 +78,7 @@ pub fn load_config<'a>(from: &Path) -> Result<Config<'a>, ConfigurationError> {
         .map_err(|e| ConfigurationError::new(e.message(), ErrorType::Error))?;
 
     // Parses 'kakfa' table
-    let kafka = config["kafka"]
-        .as_table()
-        .ok_or(ConfigurationError::new("kafka", ErrorType::TableNotFound))?;
+    let kafka = get_table("kafka", &config)?;
 
     let brokers = kafka["brokers"].as_array().ok_or(ConfigurationError::new(
         "brokers",
@@ -49,19 +88,13 @@ pub fn load_config<'a>(from: &Path) -> Result<Config<'a>, ConfigurationError> {
         .into_iter()
         .map(|x| x.as_str().unwrap_or_default().to_owned())
         .collect();
-    if brokers.contains(&String::from("")) {
-        return Err(ConfigurationError::new(
-            "",
-            ErrorType::InvalidValueForKey("brokers"),
-        ));
-    }
+
+    check_brokers(&brokers)?;
 
     let kafka = Kafka { brokers };
 
     // Parses 'data' table
-    let data = config["data"]
-        .as_table()
-        .ok_or(ConfigurationError::new("data", ErrorType::TableNotFound))?;
+    let data = get_table("data", &config)?;
 
     let source = data["source"].as_str().ok_or(ConfigurationError::new(
         "source",
@@ -69,25 +102,12 @@ pub fn load_config<'a>(from: &Path) -> Result<Config<'a>, ConfigurationError> {
     ))?;
     let source = Path::new(source);
 
-    let msg_sleep_in_ms: u64 = data["msg_sleep_in_ms"]
-        .as_integer()
-        .ok_or(ConfigurationError::new(
-            "msg_sleep_in_ms",
-            ErrorType::KeyNotFoundForTable("data"),
-        ))?
-        .try_into()
-        .map_err(|e: <u64 as TryFrom<i64>>::Error| ConfigurationError::Error(e.to_string()))?;
+    let msg_sleep_in_ms = read_integer_key_from_table("data", "msg_sleep_in_ms", data)?;
+    let msg_sleep_in_ms: u64 = from_i64_to_u64(msg_sleep_in_ms)?;
     let msg_sleep_in_ms = Duration::from_millis(msg_sleep_in_ms);
 
-    let chunk_size = data["chunk_size"]
-        .as_integer()
-        .ok_or(ConfigurationError::new(
-            "chunk_size",
-            ErrorType::KeyNotFoundForTable("data"),
-        ))?;
-    let chunk_size: u64 = chunk_size
-        .try_into()
-        .map_err(|e: <u64 as TryFrom<i64>>::Error| ConfigurationError::Error(e.to_string()))?;
+    let chunk_size = read_integer_key_from_table("data", "chunk_size", data)?;
+    let chunk_size: u64 = from_i64_to_u64(chunk_size)?;
     let chunk_size: NonZeroU64 =
         chunk_size
             .try_into()
@@ -95,27 +115,17 @@ pub fn load_config<'a>(from: &Path) -> Result<Config<'a>, ConfigurationError> {
                 ConfigurationError::Error(e.to_string())
             })?;
 
-    let chunk_sleep_in_ms: u64 = data["chunk_sleep_in_ms"]
-        .as_integer()
-        .ok_or(ConfigurationError::new(
-            "chunk_sleep_in_ms",
-            ErrorType::KeyNotFoundForTable("data"),
-        ))?
-        .try_into()
-        .map_err(|e: <u64 as TryFrom<i64>>::Error| ConfigurationError::Error(e.to_string()))?;
+    let chunk_sleep_in_ms = read_integer_key_from_table("data", "chunk_sleep_in_ms", data)?;
+    let chunk_sleep_in_ms: u64 = from_i64_to_u64(chunk_sleep_in_ms)?;
     let chunk_sleep_in_ms: Duration = Duration::from_millis(chunk_sleep_in_ms);
 
-    let dataConf = Data {
-        source: source,
+    let source = source.as_ref();
+    let data: Data = Data {
+        source,
         msg_sleep_in_ms,
         chunk_size,
         chunk_sleep_in_ms,
     };
 
-    let config = Config {
-        kafka,
-        data: dataConf,
-    };
-
-    Ok(config.clone())
+    Ok(Config { kafka, data })
 }
