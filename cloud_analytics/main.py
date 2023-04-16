cba078f3e8e94ca93217264cc26343f1dca74352
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2023-04-16 14:42:11 +0200

    spark read from each topic

--- cloud_analytics/main.py
+++ cloud_analytics/main.py
@@ -5,9 +5,34 @@ import pyspark.sql.functions as F
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType, TimestampNTZType
 
 KAFKA_BOOTSTRAP_SERVERS = "kafka:9092"
-KAFKA_TOPIC = "dataout.*"
-FILE_PATH = "/data/china/neighborhood"
+KAFKA_TOPIC = ["dataout_1", "dataout_2", "dataout_3",
+               "dataout_4", "dataout_5", "dataout_6"]
+
 OUTPUT_PATH = "/results/"
+
+
+def prepare_query(spark: SparkSession, topic_name: str):
+    df_stream = spark\
+        .readStream.format("kafka")\
+        .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)\
+        .option("subscribe", topic_name)\
+        .option("startingOffsets", "earliest")\
+        .load()
+
+    # deserialize the data from kafka
+    df_stream = df_stream.selectExpr("CAST(value AS STRING)")
+    # json deserialie using schema
+    df_stream = df_stream.select(F.from_json(
+        F.col("value"), SCHEMA).alias("data")).select("data.*")
+
+    # group by geohash and calculate the average speed of each geohash
+    gh_stream = df_stream.groupBy("geohash").agg(
+        F.avg("speed").alias("avg_speed"))
+
+    # creates a write stream with the query name
+    gh_stream.writeStream.queryName(
+        "query_" + topic_name).format("memory").outputMode("complete").start()
+
 
 SCHEMA = StructType([
     StructField("id", StringType(), False),
@@ -22,52 +47,18 @@ SCHEMA = StructType([
 spark: SparkSession = SparkSession.builder.appName(
     "write_traffic_sensor_topic").getOrCreate()
 
-df_stream = spark\
-    .readStream.format("kafka")\
-    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)\
-    .option("subscribePattern", KAFKA_TOPIC)\
-    .option("startingOffsets", "earliest")\
-    .load()
+# set log level to WARN
+spark.sparkContext.setLogLevel("WARN")
 
-# deserialize the data from kafka
-df_stream = df_stream.selectExpr("CAST(value AS STRING)")
-# json deserialie using schema
-df_stream = df_stream.select(F.from_json(
-    F.col("value"), SCHEMA).alias("data")).select("data.*")
+# create a query for each topic
+for topic in KAFKA_TOPIC:
+    prepare_query(spark, topic)
 
-# 2014-10-22T19:40:05.000Z
-df_stream = df_stream.withColumn("timestamp", F.unix_timestamp(
-    F.col("time"), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").cast(TimestampType())).withWatermark("timestamp", "1 minutes")
-
-# group by geohash and calculate the average speed of each geohash
-gh_stream = df_stream.groupBy(
-    F.col("geohash"), "timestamp").agg(F.avg("speed").alias("avg_speed"))
-# write the results to a file
-gh_stream.writeStream\
-    .queryName("geohashAvg")\
-    .format("csv")\
-    .trigger(processingTime='1 minutes')\
-    .option("checkpointLocation", "checkpoint/")\
-    .option("path", OUTPUT_PATH)\
-    .outputMode("append")\
-    .start()\
-    .awaitTermination()
-
-# print the results every 30 s
 while True:
-    x = spark.sql("SELECT * FROM geohashAvg")
-    x.head(10)
-    x.show()
-    time.sleep(30)
-
-#nb_stream = df_stream.groupBy(
-#    "neighborhood").agg(F.avg("speed").alias("avg_speed"))
-## write the results
-#nb_stream.writeStream\
-#    .queryName("neighborhoodAvg")\
-#    .format("memory")\
-#    .trigger(processingTime='1 minutes')\
-#    .option("checkpointLocation", "checkpoint/")\
-#    .outputMode("append")\
-#    .start()\
-#    .awaitTermination()
+    time.sleep(60)
+    for topic in KAFKA_TOPIC:
+        population = spark.sql("select * from query_" + topic)
+        population.show(30)
+        filename = "avg_speed_" + topic + "_" + str(time.time())
+        population.write.format("csv").option("header", "true").mode(
+            "overwrite").save(OUTPUT_PATH + filename)
