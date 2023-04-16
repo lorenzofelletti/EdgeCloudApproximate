0387fa25b6e47cf028136167d6e8babf3f9d8d7c
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2023-04-17 00:10:47 +0200

    fix map

--- flask_data_viz/geomap.py
+++ flask_data_viz/geomap.py
@@ -1,12 +1,12 @@
 import folium
-import geohash2
+import geohash
 import pandas as pd
 
 
 def process_data(latest_file_name):
     data = pd.read_csv(latest_file_name)
-    data['lat'] = data['geohash'].apply(lambda x: geohash2.decode(x)[0])
-    data['lon'] = data['geohash'].apply(lambda x: geohash2.decode(x)[1])
+    data['lat'] = data['geohash'].apply(lambda x: geohash.decode(x)[0])
+    data['lon'] = data['geohash'].apply(lambda x: geohash.decode(x)[1])
 
     # Get the maximum speed in the dataset
     max_speed = data['avg_speed'].max()
@@ -14,11 +14,17 @@ def process_data(latest_file_name):
     # Create a map centered on the first geohash in the dataset
     map = folium.Map(location=[data['lat'][0], data['lon'][0]], zoom_start=10)
 
-    for index, row in data.iterrows():
+    for _, row in data.iterrows():
         # get the color of the marker based on the average speed
         color = (row['avg_speed'] / max_speed) * 255
-        color = 'rgb(255, {}, 0)'.format(int(255 - color)) # set color based on average_speed
+        color = f'rgb({255 - color}, {color}, 0)'
 
-        folium.CircleMarker(location=[row['lat'], row['lon']], radius=15, color=color, fill=True, fill_color=color).add_to(map)
+        decoded = geohash.bbox(row['geohash'])
+        W = decoded['w']
+        E = decoded['e']
+        N = decoded['n']
+        S = decoded['s']
+
+        folium.Rectangle(bounds=[[S, W], [N, E]], color=color, fill=True, fill_color=color, popup=row['geohash']).add_to(map)
     
     map.save('templates/map.html')
