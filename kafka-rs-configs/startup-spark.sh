7c4ec3ecd35023df47eb3c0398d5a45d496bde05
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2023-04-17 00:23:41 +0200

    add flask app in spark container

--- kafka-rs-configs/startup-spark.sh
+++ kafka-rs-configs/startup-spark.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sleep 25
 
+# execute flask app in separate terminal
+bash -c "cd /flask_data_viz && source venv/bin/activate && python3 app.py" &
+
 # Set startup script
 spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 cloud_analytics/main.py
